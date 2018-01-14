a06c615399efae927c7a7d8961b49d208330a3ce
BodnariuS <bodnariusimona@yahoo.com>
2018-01-14 17:03:37 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,28 +1,33 @@
 // Select color input
 var color, row, column;
 $('#colorPicker').on('change', function(){
-color=$('#colorPicker').val();
- return color;
+var col;
+col=$('#colorPicker').val();
+color.text(col);
+ //return color;
  });
 // Select size input
 $('#input_height').on('change', function(){
- row=$('#input_height').val();
- return row;
+ var r;
+ r=$('#input_height').val();
+ row.text(r);
 });
 $('#input_width').on('change', function(){
- colom=$('#input_width').val();
- return colom;
+ var c;
+ c=$('#input_width').val();
+ colom.text(val);
 });
 
 // When size is submitted by the user, call makeGrid()
 
- function makeGrid() {
-  for (var i=1; i =< row; i++) {
+ $('#sizePicker').on('click',function makeGrid(row, colom) {
+  for (var i=1; i <= row; i++) {
       $('# pixel_canvas').append('<tr> </tr>')
-          for (var j=1; j =< colom; j++){
+          for (var j=1; j <= colom; j++){
               $('tr').append('<td> </td>')
           };
   };
 };
+                     
 // Your code goes here!
 
