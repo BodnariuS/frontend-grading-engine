bded0fd03fa9871843448b3728ae03789f143e38
BodnariuS <bodnariusimona@yahoo.com>
2018-01-14 16:02:15 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,20 +1,18 @@
 // Select color input
 var color, row, column;
-color=
+color=$('#colorPicker').val();
 // Select size input
-row=$('#input_height');
-colom=$('#input_width');
+row=$('#input_height').val();
+colom=$('#input_width').val();
 // When size is submitted by the user, call makeGrid()
 
  function makeGrid() {
-  for (var i=1, i<=row, i++) {
+  for (var i=1, i <= row, i++) {
       $('# pixel_canvas').append('<tr> </tr>')
-          for (var j=1, j<=colom, j++){
+          for (var j=1, j <= colom, j++){
               $('tr').append('<td> </td>')
-          }
-  }
-// Your code goes here!
-
-}
+          };
+  };
+};
 // Your code goes here!
 
