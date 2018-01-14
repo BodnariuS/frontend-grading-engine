52c48d120a6a8f17e486aae837f9debe6029cdbf
BodnariuS <bodnariusimona@yahoo.com>
2018-01-14 16:30:33 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -17,9 +17,9 @@ $('#input_width').on('change' function(){
 // When size is submitted by the user, call makeGrid()
 
  function makeGrid() {
-  for (var i=1, i <= row, i++) {
+  for (var i=1; i =< row; i++) {
       $('# pixel_canvas').append('<tr> </tr>')
-          for (var j=1, j <= colom, j++){
+          for (var j=1; j =< colom; j++){
               $('tr').append('<td> </td>')
           };
   };
